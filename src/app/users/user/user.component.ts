dec9a17289c7698109c8849a480229c0f7eeb471
Tanya Robertson <tanya@tanyas-imac.lan>
2020-05-05 10:18:32 -0400

    accessing route parameters & updating them

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-user',
@@ -14,10 +15,17 @@ export class UserComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.user = {
+    this.user = {  // this approach only updates when going to the component initially
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    this.route.params // this approach updates even if you are already on that component
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
